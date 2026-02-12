e3483fa5fe748176b57311a7bef97b8e3a687349
Vanessa D'Amario <vanedamario@gmail.com>
2026-02-12 17:09:04 -0500

    typo

--- run_test.sh
+++ run_test.sh
@@ -1,3 +1,4 @@
 model_list=("huatuo-o1" "meditron3-8B" )
 for model in "${model_list[@]}"; do
     python main.py --run inference --local --model $model --max_tokens 1500 --prompt_template 001
+done
